b57f58686ffbe9ad1ba990fabd46bfdfd91540df
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2023-12-03 00:20:25 +0530

    implementing share resources page

--- src/components/share/Share.jsx
+++ src/components/share/Share.jsx
@@ -1,8 +1,23 @@
 import "./share.css";
-
+import {} from "@mui/icons-material";
 const Share = () => {
   return (
-    <div>Share</div>
+    <div className="share">
+        <div className="shareWrapper">
+            <div className="shareTop">
+                <img src="/assets/person/1.jpeg" alt="" className="shareProfileImg" />
+                <input placeholder="What you thinkin" className="shareInput" />
+            </div>
+            <hr className="shareHr" />
+            <div className="shareBottom">
+                <div className="shareOptions">
+                    <div className="shareOption">
+                        <span className="shareOptionText">Photo or Video</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
