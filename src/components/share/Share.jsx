461fc85b1f13ef4b3ec17886a4172bf04f439653
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2023-12-03 00:44:30 +0530

    Share post buttons

--- src/components/share/Share.jsx
+++ src/components/share/Share.jsx
@@ -1,5 +1,5 @@
 import "./share.css";
-import {} from "@mui/icons-material";
+import {PermMediaIcon, LabelIcon, RoomIcon, EmojiEmotionsIcon} from "@mui/icons-material";
 const Share = () => {
   return (
     <div className="share">
@@ -12,8 +12,22 @@ const Share = () => {
             <div className="shareBottom">
                 <div className="shareOptions">
                     <div className="shareOption">
-                        <span className="shareOptionText">Photo or Video</span>
+                        <PermMediaIcon className="shareIcon"/>
+                        <span className="shareOptionText" htmlColor="tomato">Photo or Video</span>
                     </div>
+                    <div className="shareOption">
+                        <LabelIcon className="shareIcon"/>
+                        <span className="shareOptionText" htmlColor="blue">Tag</span>
+                    </div>
+                    <div className="shareOption">
+                        <RoomIcon className="shareIcon"/>
+                        <span className="shareOptionText" htmlColor="green">Location</span>
+                    </div>
+                    <div className="shareOption">
+                        <EmojiEmotionsIcon className="shareIcon"/>
+                        <span className="shareOptionText" htmlColor="goldenrod">Emotions</span>
+                    </div>
+                    <button className="shareButton">Share</button>
                 </div>
             </div>
         </div>
