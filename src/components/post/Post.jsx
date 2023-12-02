6aca17fe915b4d1907e39fc6d859c1a373833451
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2023-12-03 01:03:02 +0530

    creating posts

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
-
+import {MoreVertIcon} from '@mui/icons-material'
 const Post = () => {
   return (
-    <div>Post</div>
+    <div className='post'>
+      <div className="postWrapper">
+      <div className="postTop">
+        <div className="postTopLeft">
+          <img src="/assets/pesron/1.jpeg" alt="" className="postProfileImg" />
+          <div className="postUsername">King</div>
+          <div className="postDate">5 mins ago</div>
+        </div>
+        <div className="postTopRight">
+          <MoreVertIcon/>
+        </div>
+      </div>
+      <div className="postCenter">
+        <span className="postText">Hey its my first post</span>
+        <img src="/assets/post/1.jpeg" alt="" className="postImg" />
+      </div>
+      <div className="postBottom">
+        <div className="postBottomLeft">
+          <img src="/assets/like.png" alt="" className="likeIcon" />
+          <img src="/assets/heart.png" alt="" className="likeIcon" />
+          <span className="postLikeCounter">32 people like it</span>
+        </div>
+        <div className="postBottomRight">
+          <span className="postCommentText">9 comments</span>
+        </div>
+      </div>
+      </div>
+    </div>
   )
 }
 
