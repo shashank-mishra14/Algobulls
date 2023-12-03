639eef505c4d5f7a313097db3a18409edadb3611
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2023-12-03 16:08:29 +0530

    mapping data on the site with the dummy data

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,31 +1,37 @@
 import React from 'react'
 import {MoreVertIcon} from '@mui/icons-material'
-const Post = () => {
+import {Users} from '../../dummyData'
+
+const Post = ({post}) => {
+
+
   return (
     <div className='post'>
       <div className="postWrapper">
       <div className="postTop">
         <div className="postTopLeft">
-          <img src="/assets/pesron/1.jpeg" alt="" className="postProfileImg" />
-          <div className="postUsername">King</div>
-          <div className="postDate">5 mins ago</div>
+          <img src={Users.filter((u) => u.id === post?.userId)[0].profilePicture} alt="" className="postProfileImg" />
+          <span className="postUsername">
+              {Users.filter((u) => u.id === post?.userId)[0].username}
+            </span>
+          <span className="postDate">{post.date}</span>
         </div>
         <div className="postTopRight">
           <MoreVertIcon/>
         </div>
       </div>
       <div className="postCenter">
-        <span className="postText">Hey its my first post</span>
-        <img src="/assets/post/1.jpeg" alt="" className="postImg" />
+        <span className="postText">{post?.desc}</span>
+        <img src={post.photo} alt="" className="postImg" />
       </div>
       <div className="postBottom">
         <div className="postBottomLeft">
           <img src="/assets/like.png" alt="" className="likeIcon" />
           <img src="/assets/heart.png" alt="" className="likeIcon" />
-          <span className="postLikeCounter">32 people like it</span>
+          <span className="postLikeCounter">{post.like} people like it</span>
         </div>
         <div className="postBottomRight">
-          <span className="postCommentText">9 comments</span>
+          <span className="postCommentText">{post.comment} comments</span>
         </div>
       </div>
       </div>
