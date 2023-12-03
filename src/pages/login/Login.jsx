66a39771d2c6f7ff18241c93212f63623bd4e3e3
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2023-12-03 19:53:50 +0530

    login page

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './login.css'
+
+const Login = () => {
+  return (
+    <div className='login'>
+        <div className="loginWrapper">
+            <div className="loginLeft">
+
+            </div>
+            <div className="loginRight"></div>
+        </div>
+    </div>
+  )
+}
+
+export default Login
