a1151763490e4c1faa053786089f0cdde7146d37
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2023-12-03 16:27:28 +0530

    changes to the sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,75 +1,60 @@
 import "./sidebar.css";
-import {RssFeedIcon, ChatsIcon, VideosIcon, GroupsIcon, BookmarksIcon, QuestionsIcon, JobsIcon, EventsIcon, CoursesIcon} from "@mui/icons-material";
+import {RssFeedIcon, ChatsIcon,PlayCircleFilledOutlinedIcon, HelpOutlineIcon, GroupsIcon, BookmarksIcon, WorkOutlineIcon, EventIcon, SchoolIcon} from "@mui/icons-material";
 
-const Sidebar = () => {
+import { Users } from "../../dummyData";
+import CloseFriend from "../closeFriend/CloseFriend";
+
+const Sidebar=()=> {
   return (
     <div className="sidebar">
       <div className="sidebarWrapper">
         <ul className="sidebarList">
           <li className="sidebarListItem">
-            <RssFeedIcon className="sidebarIcon"/>
+            <RssFeedIcon className="sidebarIcon" />
             <span className="sidebarListItemText">Feed</span>
           </li>
           <li className="sidebarListItem">
-            <ChatsIcon className="sidebarIcon"/>
+            <ChatsIcon className="sidebarIcon" />
             <span className="sidebarListItemText">Chats</span>
           </li>
           <li className="sidebarListItem">
-            <VideosIcon className="sidebarIcon"/>
-            <span className="sidebarListItemText">Vidoes</span>
+            <PlayCircleFilledOutlinedIcon className="sidebarIcon" />
+            <span className="sidebarListItemText">Videos</span>
           </li>
           <li className="sidebarListItem">
-            <GroupsIcon className="sidebarIcon"/>
+            <GroupsIcon className="sidebarIcon" />
             <span className="sidebarListItemText">Groups</span>
           </li>
           <li className="sidebarListItem">
-            <BookmarksIcon className="sidebarIcon"/>
+            <BookmarksIcon className="sidebarIcon" />
             <span className="sidebarListItemText">Bookmarks</span>
           </li>
           <li className="sidebarListItem">
-            <QuestionsIcon className="sidebarIcon"/>
+            <HelpOutlineIcon className="sidebarIcon" />
             <span className="sidebarListItemText">Questions</span>
           </li>
           <li className="sidebarListItem">
-            <JobsIcon className="sidebarIcon"/>
+            <WorkOutlineIcon className="sidebarIcon" />
             <span className="sidebarListItemText">Jobs</span>
           </li>
           <li className="sidebarListItem">
-            <EventsIcon className="sidebarIcon"/>
+            <EventIcon className="sidebarIcon" />
             <span className="sidebarListItemText">Events</span>
           </li>
           <li className="sidebarListItem">
-            <CoursesIcon className="sidebarIcon"/>
+            <SchoolIcon className="sidebarIcon" />
             <span className="sidebarListItemText">Courses</span>
           </li>
         </ul>
         <button className="sidebarButton">Show More</button>
-        <hr className="sidebarHr"/>
+        <hr className="sidebarHr" />
         <ul className="sidebarFriendList">
-          <li className="sidebarFriend">
-            <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
-            <span className="sidebarFriendName">Moye Moye</span>
-          </li>
-          <li className="sidebarFriend">
-            <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
-            <span className="sidebarFriendName">Moye Moye</span>
-          </li>
-          <li className="sidebarFriend">
-            <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
-            <span className="sidebarFriendName">Moye Moye</span>
-          </li>
-          <li className="sidebarFriend">
-            <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
-            <span className="sidebarFriendName">Moye Moye</span>
-          </li>
-          <li className="sidebarFriend">
-            <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
-            <span className="sidebarFriendName">Moye Moye</span>
-          </li>
+          {Users.map((u) => (
+            <CloseFriend key={u.id} user={u} />
+          ))}
         </ul>
       </div>
     </div>
-  )
+  );
 }
-
 export default Sidebar
