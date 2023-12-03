80ef96c262f3823a3c696214ff1a799e39276159
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2023-12-04 02:50:20 +0530

    setting up the cluster

--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+
 const express = require('express');
 const app= express();
-app.listen(8800, () => {
-  console.log("server is ready")
-});
+
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+mongoose.connect(process.env.MONGO_URL,{useNewUrlParser: true, useUnifiedTopology:true},()=>{console.log('Connected to MongoDB')});
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
