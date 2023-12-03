431b946cf79793bcdde3d0d6d806f544cb473c6e
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2023-12-04 02:32:25 +0530

    adding the server

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+const express = require('express');
+const app= express();
+app.listen(8800, () => {
+  console.log("server is ready")
+});
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
