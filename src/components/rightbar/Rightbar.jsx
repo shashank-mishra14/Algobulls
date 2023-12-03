e17a41a8b19aedb59a43a2ab13ba284c0e4fd557
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2023-12-03 15:47:32 +0530

    Creating components

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -2,7 +2,16 @@ import "./rightbar.css"
 
 const Rightbar = () => {
   return (
-    <div className="rightbar">Rightbar</div>
+    <div className="rightbar">
+      <div className="rightbarWrapper">
+        <div className="birthdayContainer">
+          <img src="assets/gift.png" alt="" className="birthdayImg" />
+          <span className="birthdayText">
+            <b> Ashley</b> and <b>3 others</b> have their bdays today
+            </span>
+        </div>
+      </div>
+    </div>
   )
 }
 export default Rightbar
