19c623832d1ae004afbc90025cf07dc9d1b7f833
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2023-12-17 01:21:38 +0530

    making changes in the schema

--- src/Models/Post.js
+++ src/Models/Post.js
@@ -1,59 +1,20 @@
 const mongoose = require("mongoose");
 
 const PostSchema = new mongoose.Schema({
-    username: {
+    userId: {
         type: String,
         required: true,
-        min: 3,
-        max: 20,
-        unique: true,
     },
-    email: {
+    desc: {
         type: String,
-        required: true,
-        max: 50,
-        unique: true,
+        max: 500,
     },
-    password: {
+    img: {
         type: String,
-        required: true,
-        min: 6,
     },
-    profilePicture: {
-        type: String,
-        default: "",
-    },
-    coverPicture: {
-        type: String,
-        default: "",
-    },
-    followers:{
+    likes: {
         type: Array,
         default: [],
-    },
-    following:{
-        type: Array,
-        default: [],
-    },
-    isAdmin:{
-        type: Boolean,
-        default: false,
-    },
-    desc:{
-        type: String,
-        max: 50,
-    },
-    city:{
-        type: String,
-        max: 50,
-    },
-    from:{
-        type: String,
-        max: 50,
-    },
-    relationship:{
-        type: Number,
-        enum: [1,2,3],
     },
     
 },
