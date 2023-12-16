642ae71f104f6f80bd006f20284a5e8767216712
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2023-12-17 01:02:42 +0530

    deleting a user

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,30 +4,40 @@ const bcrypt = require("../../models/bcrypt");
 
 //update a user
 router.put("/:id", async (req, res) => {
-  if(req.body.userId === req.params.id || req.body.userIsAdmin){
-    if(req.body.password){
-      try{
+  if (req.body.userId === req.params.id || req.body.userIsAdmin) {
+    if (req.body.password) {
+      try {
         const salt = await bcrypt.genSalt(10);
         req.body.password = await bcrypt.hash(req.body.password, salt);
-      }catch(err){
+      } catch (err) {
         return res.status(500).json(err);
       }
     }
-    try{
+    try {
       const user = await User.findByIdAndUpdate(req.params.id, {
         $set: req.body,
       });
       res.status(200).json("account has been updated");
-  }
-  catch(err){
-    return res.status(500).json(err);
-  }
-}
-  else{
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
     return res.status(403).json("you can update only your account");
   }
 });
 //delete a user
+router.delete("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.userIsAdmin) {
+    try {
+      const user = await User.findByIdAndDelete(req.params.id);
+      res.status(200).json("account has been deleted");
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res.status(403).json("you can delete only your account");
+  }
+});
 //get a user
 //follow a user
 //unfollow a user
