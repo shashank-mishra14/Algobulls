99b542cbd3f316d39c8c5e58e7b6b035b7af6ee0
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2023-12-16 14:44:12 +0530

    creating hash of the password

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,21 +1,23 @@
 const router = require("express").Router();
-const User= require("../../models/User");
+const User = require("../../models/User");
+const bcrypt = require("../../models/bcrypt");
 
 //register
 router.post("/register", async (req, res) => {
-   const newUser = new User({
-         username: req.body.username,
-         email: req.body.email,
-         password: req.body.password,
+  try {
+    //generate new password and hash it
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
     });
-
-    try{
-        const user = await newUser.save();
-        res.status(200).json(user);
-    }
-    catch(err){
-        console.log(err);
-    }
+    const user = await newUser.save();
+    res.status(200).json(user);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 module.exports = router;
