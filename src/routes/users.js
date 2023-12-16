d735285d2acb91649fbf569c09dffe98e6a09a8d
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2023-12-17 01:05:26 +0530

    update a user

--- src/routes/users.js
+++ src/routes/users.js
@@ -39,6 +39,15 @@ router.delete("/:id", async (req, res) => {
   }
 });
 //get a user
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, updatedAt, ...other } = user._doc;
+    res.status(200).json(other);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
 //follow a user
 //unfollow a user
 
