b57d02446d2a6ef6746cc0954a3d99a316bb9e9b
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2023-12-17 15:36:10 +0530

    creating a post api

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -1,6 +1,19 @@
 const router = require('express').Router();
+const Post = require("../models/Post");
 
-router.get('/', async (req, res) => {
-    res.send('This is the user route');
-});
+//create a post
+router.post("/", async function(req, res) {
+    const newPost = new Post(req.body);
+    try{
+        const savedPost = await newPost.save();
+        res.status(200).json(savedPost);
+    }catch(err){
+        res.status(500).json(err);
+    }
+}); 
+//update a post
+//delete a post
+//like a post
+//get a post
+//get timeline posts
 module.exports = router;
