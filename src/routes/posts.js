53946076d63f6f85e0fbb196639f8df67cd89c31
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2023-12-17 16:01:32 +0530

    like and dislike a post

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -40,6 +40,20 @@ router.delete("/:id", async function(req, res) {
 }
 });
 //like a post
+router.post("/:id/like", async function(req, res) {
+    try{
+        const post = await Post.findById(req.params.id);
+        if(!post.likes.includes(req.body.userId)){
+            await post.updateOne({$push: {likes: req.body.userId}});
+            res.status(200).json("The post has been liked");
+        }else{
+            await post.updateOne({$pull: {likes: req.body.userId}});
+            res.status(200).json("The post has been disliked");
+        }
+    }catch(err){
+        res.status(500).json(err);
+    }
+});
 //get a post
 //get timeline posts
 module.exports = router;
